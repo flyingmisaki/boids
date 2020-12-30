8c11b10b5633f61a8965bdc60e3f11d957635c78
flyingmisaki <pierrick@live.co.uk>
2020-12-30 18:45:16 +0100

    Initial commit for boids. Alignement

---
+++ boid.js
@@ -0,0 +1,51 @@
+class Boid {
+    constructor() {
+        this.position = createVector(random(width), random(height))
+        this.velocity = p5.Vector.random2D()
+        this.velocity.setMag(random(2, 4))
+        this.acceleration = createVector()
+    }
+
+    align(boids) {
+        let perceptionRadius = 50
+        let steering = createVector()
+        let total = 0
+        
+        for (let other of boids) {
+            let d = dist(
+                this.position.x, 
+                this.position.y, 
+                other.position.x, 
+                other.position.y
+            )
+            
+            if ( other != this && d < perceptionRadius) {
+                steering.add(other.velocity)
+                total++
+            }
+        }
+        
+        if (total > 0) {
+            steering.div(total)
+            steering.sub(this.velocity)
+        }
+        return steering
+    }
+
+    flock(boids) {
+        let alignment = this.align(boids)
+        this.acceleration = alignment
+    }
+
+    // Updates the pos
+    update() {
+        this.position.add(this.velocity)
+        this.velocity.add(this.acceleration)
+    }
+
+    show() {
+        strokeWeight(8)
+        stroke(255)
+        point(this.position.x, this.position.y)
+    }
+}
